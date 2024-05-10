410c93ebcce957f320c65b5d886037fb1ebfab85
NikolayB <nikolayb0905@gmail.com>
2024-05-11 00:32:39 +0300

    add theme, router and base url for axios

--- src/index.js
+++ src/index.js
@@ -1,13 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import App from "./App";
+import Dashboard from "./Pages/Dashboard";
+import { BACKEND_URL } from "./constants";
+
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import axios from "axios";
+
+import "./index.css";
+
+axios.defaults.baseURL = BACKEND_URL;
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#7734e7",
+      light: "#cd79f5",
+    },
+    secondary: {
+      main: "#0132fc",
+      dark: "#010101",
+      white: "#fffeff",
+      grey: "#F0F3FF",
+      zodiac: "#12375c",
+      pink: "#f1a091",
+      tia: "#cc4f0e",
+      sinbad: "#94ced3",
+    },
+  },
+});
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
