72ec8f583f862bcfec635704890fb606417922bc
NikolayB <nikolayb0905@gmail.com>
2024-05-11 00:32:57 +0300

    create constants and utility functions

---
+++ src/utils.js
@@ -0,0 +1,7 @@
+export const parseTime = (date) => {
+  const hours = new Date(date).getHours();
+  const minutes = new Date(date).getMinutes();
+  return `${hours > 9 ? hours : `0${hours}`}:${
+    minutes > 9 ? minutes : `0${minutes}`
+  }`;
+};
